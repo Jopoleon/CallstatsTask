0ab9b75943767302a6debdbedf7f9801cc8075f6
Egor Miloserdov <Egor_Miloserdov@epam.com>
2018-01-30 16:26:29 +0300

    fixes for first review remarks: strings change to ints, add sorting for odd length of slide window

--- windows.go
+++ windows.go
@@ -12,6 +12,7 @@ import (
 	"io/ioutil"
 
 	"fmt"
+	"sort"
 )
 
 const (
@@ -21,11 +22,11 @@ const (
 
 type DelayCalculator interface {
 	AddInterval(value int)
-	GetMedian() string
+	GetMedian() int
 }
 
 type Window struct {
-	arr      []string
+	arr      []int
 	size     int
 	head     int
 	tail     int
@@ -37,7 +38,7 @@ func New(size, interval int) *Window {
 		panic("Must have positive size and interval")
 	}
 	return &Window{
-		arr:      make([]string, size),
+		arr:      make([]int, size),
 		size:     size,
 		interval: interval,
 	}
@@ -52,12 +53,12 @@ type Res struct {
 	position int
 }
 
-func (w *Window) GetMedian() string {
-	var r string
+func (w *Window) GetMedian() int {
+	var r int
 	a := w.Slice()
 	if len(a) == 1 {
 
-		return "-1"
+		return -1
 	}
 	//if len is even (M) = value of [((n)/2)th item term + ((n)/2 + 1)th item term ] /2
 	if l := len(a); l%2 == 0 {
@@ -66,23 +67,17 @@ func (w *Window) GetMedian() string {
 		if err != nil {
 			panic(err)
 		}
-	} else { //if len is odd (M) = value of ((n + 1)/2)th
+	} else {
+		//If n is odd then Median (M) = value of ((n + 1)/2)th item from a sorted array of length n.
+		sort.Ints(a)
 		r = a[(len(a)-1)/2]
 	}
 
 	return r
 }
 
-func evenLen(a, b string) (median string, err error) {
-	c1, err := strconv.Atoi(a)
-	if err != nil {
-		return "", err
-	}
-	c2, err := strconv.Atoi(b)
-	if err != nil {
-		return "", err
-	}
-	median = strconv.Itoa((c1 + c2) / 2)
+func evenLen(a, b int) (median int, err error) {
+	median = (a + b) / 2
 	return median, nil
 }
 
@@ -91,19 +86,21 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	//Sliding window size for Test 2 is 100
+	//Sliding window size for Test 3 is 1000
+	//Sliding window size for Test 4 is 10000
+	var sizeStep = 100
 	for _, f := range files {
 		lines, err := readCSV("./getMedian-attachments/" + f.Name())
 		if err != nil {
 			log.Fatal(err)
 		}
 		start := time.Now()
-		output := make(chan Res)
-		defer close(output)
-		win := New(size, interval)
+		win := New(sizeStep, interval)
 		var resultArray []string
 		for i := 0; i < len(lines); i++ {
 			win.Push(lines[i])
-			resultArray = append(resultArray, win.GetMedian()+"\r\n")
+			resultArray = append(resultArray, strconv.Itoa(win.GetMedian())+"\r\n")
 		}
 		endTime := time.Since(start)
 		f, err := os.Create(f.Name() + "output.txt")
@@ -113,32 +110,37 @@ func main() {
 		for _, s := range resultArray {
 			fmt.Fprintf(f, s)
 		}
-		log.Println(f.Name()+" Sync Result time: ", endTime)
+		log.Println(f.Name()+", \n Slide window size:", sizeStep, " \n Sync Result time: ", endTime)
+		sizeStep = sizeStep * 10
 	}
 
 }
 
-func readCSV(path string) ([]string, error) {
+func readCSV(path string) ([]int, error) {
 	file, err := os.Open(path)
 	if err != nil {
-		return []string{}, err
+		return []int{}, err
 	}
 
 	reader := csv.NewReader(file)
-	var lines []string
+	var lines []int
 	for {
 		record, err := reader.Read()
 		if err == io.EOF {
 			break
 		}
 		if err == nil && len(record) > 0 {
-			lines = append(lines, strings.Trim(record[0], "\r"))
+			i, err := strconv.Atoi(strings.Trim(record[0], "\r"))
+			if err != nil {
+				return []int{}, err
+			}
+			lines = append(lines, i)
 		}
 	}
 	return lines, nil
 }
 
-func (w *Window) Push(v string) {
+func (w *Window) Push(v int) {
 	if w.tail == len(w.arr) {
 		w.swap()
 	}
@@ -157,6 +159,6 @@ func (w *Window) swap() {
 	w.head, w.tail = 0, w.size-1
 }
 
-func (w *Window) Slice() []string {
+func (w *Window) Slice() []int {
 	return w.arr[w.head:w.tail]
 }
